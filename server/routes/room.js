31ccfa3db01b79f6a6328beba4beeb5f8e03352b
Jonas Schütz <jonas.schuetz@students.fhnw.ch>
2019-07-07 14:21:52 +0200

    GET request for all rooms created.

--- server/routes/room.js
+++ server/routes/room.js
@@ -4,6 +4,10 @@ var router = express.Router();
 
 router.use(express.json());
 
+router.get('/', function(req, res) {
+    models.room.findAll().then((result) => res.json(result))
+});
+
 router.get('/:id', function(req, res) {
     models.room.findOne({ where: { id: req.params.id } }).then((result) => res.json(result));
 });
