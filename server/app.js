bbf1ba5fa7586669b66f8d67588bdfac4cb21f64
Jonas Schütz <jonas.schuetz@students.fhnw.ch>
2019-07-13 10:11:45 +0200

    typo fix.

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join('apo/client/build')));
+app.use(express.static(path.join('app/client/build')));
 
 app.use('/api/', routes);
 app.use('/api/stay', stayRouter);
