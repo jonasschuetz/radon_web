72da5204dceb83815d4b005351853f12f59b2e85
Jonas Schütz <jonas.schuetz@students.fhnw.ch>
2019-07-13 09:52:12 +0200

    changes to app.js for deployment.

--- server/app.js
+++ server/app.js
@@ -13,10 +13,6 @@ var empRouter = require('./routes/employee');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,25 +23,9 @@ app.use('/api/stay', stayRouter);
 app.use('/api/room', roomRouter);
 app.use('/api/employee', empRouter);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build', 'index.html'));
 });
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render(err.message);
-});
 
 module.exports = app;
