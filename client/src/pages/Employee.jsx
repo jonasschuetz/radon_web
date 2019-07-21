06d20a8cf639af31651cd36e93b3a9292b301c2d
Jonas Schütz <jonas.schuetz@students.fhnw.ch>
2019-07-21 10:45:46 +0200

    serveral front end changes.

--- client/src/pages/Employee.jsx
+++ client/src/pages/Employee.jsx
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import '../App.css';
 
 
 class Employee extends Component {
@@ -13,56 +14,100 @@ class Employee extends Component {
     constructor(props){
         super(props);
         this.state = {
-            'stays': [],
-            emp: 0
+            stays: [],
+            rooms: [],
+            emp: 0,
+            durations: {}            
         }
     }
 
 
     componentDidMount(){
-        this.getList();
-        this.getEmpList();
+        this.getStays();
+        this.getEmp();
+        this.getRoom();
     }
 
-    getEmpList = () => {
+    getEmp = () => {
         var id = this.props.match.params.id;
         fetch(`/api/employee/${id}`)
         .then(res => res.json())
         .then(result => this.setState({emp: result }))
     }  
 
-    getList = () => {
+    getRoom = () => {
+        fetch('/api/room')
+        .then(res => res.json())
+        .then(result => this.setState({rooms: result }))
+    }
+
+    getStays = () => {
         var id = this.props.match.params.id;
         fetch(`/api/stay/employee/${id}`)
         .then(res => res.json())
-        .then(results => this.setState({ 'stays': results }))
+        .then(results => this.calulateDuration(results))
+    }
+
+    calulateDuration = (stays) => {
+       var workingStays = stays;
+       var durationsMap = {};
+       for(var s in workingStays){
+            var startDate = new Date(workingStays[s].startTime);
+            var endDate = new Date(workingStays[s].endTime);
+            var duration = startDate.getSeconds() - endDate.getSeconds();
+            durationsMap[workingStays[s].id] = duration;
+       }
+       this.setState({stays: stays});
+       this.setState({durations: durationsMap});
+
     }
 
     render() {
+        var rooms = this.state.rooms;
+        var durations = this.state.durations;
+        console.log();
         return(
             <div className="App">
                 <Navbar/>
                 <Container className="radonContainer">
                 <Row>   
                 <Col>
+                <div className="spacer" />
                 <h1>{this.state.emp.firstName} {this.state.emp.lastName}</h1>
                 <p>ID: {this.props.match.params.id}</p>
-                
+                <div className="spacer" />
+                <div>
+                    <p className="RadonText">Stunden:</p>
+
+                </div>
+                <div className="spacer" />
                 <ul>
+                    <li>
                     {this.state.stays.map(function(stay, index) {
                         return(
                             <div key={index}>
                             <div className="FlacheRadonCard">
+                                <div className = "FlacheRadonCard-Links">
+                                
                                 <p className="StayAnlagenamen">Aufenthalt {stay.id}</p>
-                                <p className="RadonText">{stay.startTime}</p>
-                                <p className="RadonText">{stay.endTime}</p>
-                                <p className="RadonText">{stay.dose}</p>
+                                <p className="stayText">Raum: {stay.roomId}</p>  
+                                <p className="stayText">Aufenthaltsdauer: {durations[stay.id]}</p>
+                                
+                                
+                                </div>
+
+                                <div className="FlacheRadonCard-Rechts">
+                                <p className="dosisText">{stay.dose}</p>
+                                <p className="dosisLabel">Bq / m3</p>                                
+                                </div>
                             </div>  
                             </div>
                            
                         ) 
                     }
                     )}
+                    </li>
+                    
                 </ul>
                 <div>
                 <Link to="/">
