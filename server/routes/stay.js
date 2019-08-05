058ab5da0edaafdb63491f8bcda18a8356785be7
Jonas Schütz <jonas.schuetz@students.fhnw.ch>
2019-08-05 19:03:01 +0200

    commit fix date time issue.

--- server/routes/stay.js
+++ server/routes/stay.js
@@ -2,11 +2,19 @@ var models = require('../models/index');
 var express = require('express');
 var router = express.Router();
 var http = require('http');
+var moment = require('moment');
+const { Op } = require('sequelize');
 
 router.use(express.json());
 
 router.get('/', function(req, res) {
-    models.stay.findAll().then((result) => res.json(result))
+    models.stay.findAll({
+        where: {
+            startTime: {
+                [Op.gte]: moment().subtract(1, 'years').toDate()
+            }
+        }
+    }).then((result) => res.json(result))
 });
 
 //TODO: Im Bericht erklären, zusammen mit updateEmployeeDosis. 
@@ -24,14 +32,26 @@ router.post('/create', function(req, res) {
 });
 
 router.get('/employee/:id', function(req, res) {
-    models.stay.findAll({ where: { employeeId: req.params.id } }).then((result) => res.json(result));
+    models.stay.findAll({
+        where: {
+            employeeId: req.params.id,
+            startTime: {
+                [Op.gte]: moment().subtract(1, 'years').toDate()
+            }
+        }
+    }).then((result) => res.json(result));
 });
 
 
 
 const updateEmployeeDosis = empId => {
     models.stay.findAll({
-        where: { employeeId: empId }
+        where: {
+            employeeId: req.params.id,
+            startTime: {
+                [Op.gte]: moment().subtract(1, 'years').toDate()
+            }
+        }
     }).then(stays => {
             for (s in stays) {
                 dosisSum = dosisSum + parseFloat(stays[s].dose);
